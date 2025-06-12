be2266cfafc2202f6352b71393c8b17b7f0782ec
sheva <75427781+sheva2250@users.noreply.github.com>
2025-06-12 14:50:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,11 @@ def image_to_base64(buf):
     return base64.b64encode(buf.getvalue()).decode()
 
 # === UI Streamlit ===
+st.markdown("Nama: Sheva Aqila Ramadhan - 2304130083")
+st.markdown("Dikerjakan oleh saya sendiri dan 10% AI")
+
+st.divider()
+
 st.title("Zero-Shot Object Detection")
 
 model_option = st.selectbox("Pilih Model", ["Grounding DINO", "OWL-V2"])
